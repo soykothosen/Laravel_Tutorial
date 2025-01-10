47ec3b9fd05b5a4c882b8943df3132d1dd6ef16a
Soykot Hosen <soykothosen1603028@gmail.com>
2025-01-10 10:46:41 +0600

    upto basic setup and routing

---
+++ project-name/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/laravel', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('custom');
+});
